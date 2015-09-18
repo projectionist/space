050196f76450653ec8b0c72309ead7c297322660
Sam Taylor <sjltaylor@gmail.com>
2015-09-17 20:42:38 -0400

    signal handling just works

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,8 @@ using namespace std;
 
 static shared_ptr<projection::projection> p = make_shared<projection::projection>();
 
-void handle_signal(int signal)
-{
-  switch (signal) {
-    case SIGHUP:
-    case SIGINT:
-    case SIGQUIT:
-    case SIGKILL:
-      p->stop();
-  }
-}
-
-void handle_signals()
-{
-  struct sigaction sigint;
-  sigint.sa_handler = &handle_signal;
-  sigemptyset(&sigint.sa_mask);
-  sigint.sa_flags = SA_RESETHAND;
-  sigaction(SIGINT, &sigint, NULL);
-}
-
 int main()
 {
-  auto p = make_shared<projection::projection>();
-
   p->run();
 
   return EXIT_SUCCESS;
