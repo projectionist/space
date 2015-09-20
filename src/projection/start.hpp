0fab60efc26cc5f40d5b0d36e761cb66a3a2b0d5
Sam Taylor <sjltaylor@gmail.com>
2015-09-20 10:20:05 -0400

    heap allocate the display

--- src/projection/start.hpp
+++ src/projection/start.hpp
@@ -17,30 +17,32 @@ using namespace std;
 namespace projection {
 
   void play(scratch::scratch s) {
-    struct display display;
+    struct display *display = (struct display *) malloc(sizeof(struct display));
+    assert(display);
 
-    display.display = wl_display_connect(NULL);
-    assert(display.display);
+    display->display = wl_display_connect(NULL);
+    assert(display->display);
 
-    init_registry(&display);
+    init_registry(display);
 
-    wl_display_dispatch(display.display);
+    wl_display_dispatch(display->display);
 
-    init_egl(&display);
-    create_surface(&display);
-    init_gl(&display);
+    init_egl(display);
+    create_surface(display);
+    init_gl(display);
 
     int ret = 0;
-    while (!display.stop_flag) {
-      ret = wl_display_dispatch(display.display);
+    while (!display->stop_flag) {
+      ret = wl_display_dispatch(display->display);
       if (ret == -1) break;
     }
 
-    destroy_surface(&display);
-    fini_egl(&display);
+    destroy_surface(display);
+    fini_egl(display);
 
-    destroy_registry(&display);
-    wl_display_flush(display.display);
-    wl_display_disconnect(display.display);
+    destroy_registry(display);
+    wl_display_flush(display->display);
+    wl_display_disconnect(display->display);
+    free(display);
   }
 }
