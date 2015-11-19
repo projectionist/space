ef0902032b4d0aa06a0628db2fcde9743838cdc5
Sam Taylor <sjltaylor@gmail.com>
2015-11-19 17:39:03 -0500

    remove bars, draw nothing

--- src/scratch/scratch.hpp
+++ src/scratch/scratch.hpp
@@ -13,8 +13,6 @@ namespace scratch {
   class scratch : public projection::drawing {
     std::unique_ptr<projection::program_helper> program_helper;
     GLuint program;
-    std::vector<GLfloat> bars;
-    void generate_bars();
   public:
     void setup();
     void draw();
