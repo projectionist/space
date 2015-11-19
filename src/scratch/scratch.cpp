260d088a4d03829b4096b999193cc37a6078f8a1
Sam Taylor <sjltaylor@gmail.com>
2015-11-19 17:39:03 -0500

    remove bars, draw nothing

--- src/scratch/scratch.cpp
+++ src/scratch/scratch.cpp
@@ -27,66 +27,13 @@ namespace scratch {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glUseProgram(program);
-
-    generate_bars();
-  }
-
-  void scratch::generate_bars()
-  {
-    vector<GLfloat> vertices;
-
-    const int steps = 16; // twice the number of bars (bar, gap...)
-    // normalized description of bar heights and gap heights
-    //bar, gap, bar, gap etc.
-    GLfloat description[steps] =
-    { 0.2841398731256567, 0.056827974625131346,
-      0.11365594925026269, 0.056827974625131346,
-      0.056827974625131346, 0.056827974625131346,
-      0.03342822036772432, 0.056827974625131346,
-      0.021856913317358208, 0.056827974625131346,
-      0.01535891206084631, 0.056827974625131346,
-      0.011365594925026269, 0.056827974625131346,
-      0.008742765326943285, 0.056827974625131346 };
-
-    GLfloat y = 1.0f;
-
-    for(int bar_idx = 0, step_idx = 1; bar_idx < steps; bar_idx += 2, step_idx += 2) {
-      GLfloat height = description[bar_idx ] * 2;
-      GLfloat gap    = description[step_idx] * 2;
-
-      // GL_TRIANGLE_STRIP
-
-      // top left
-      vertices.push_back(-1.0f);
-      vertices.push_back(y);
-      vertices.push_back(0.0f);
-
-      // bottom left
-      vertices.push_back(-1.0f);
-      vertices.push_back(y - height);
-      vertices.push_back(0.0f);
-
-      // top right
-      vertices.push_back(1.0f);
-      vertices.push_back(y);
-      vertices.push_back(0.0f);
-
-      // bottom right
-      vertices.push_back(1.0f);
-      vertices.push_back(y - height);
-      vertices.push_back(0.0f);
-
-      y -= height + gap;
-    }
-
-    bars = vertices;
   }
 
   void scratch::draw()
   {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    GLfloat *vertices = &bars[0];
+    GLfloat vertices[0];
 
     // Set the viewport
     glViewport(0, 0, width(), height());
@@ -100,11 +47,9 @@ namespace scratch {
 
     auto a_pos_location = glGetAttribLocation(program, "a_position");
     // Load the vertex data
-    glVertexAttribPointer(a_pos_location, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+    glVertexAttribPointer(a_pos_location, 2, GL_FLOAT, GL_FALSE, 0, vertices);
     glEnableVertexAttribArray(a_pos_location);
 
-    for (unsigned int i = 0; i < bars.size() / 3; i += 4) {
-      glDrawArrays(GL_TRIANGLE_STRIP, i, 4);
-    }
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 0);
   }
 }
